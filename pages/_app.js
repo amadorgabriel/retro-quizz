6dc9da94f31ca3896f0dedb73bb6f6d5f1754a52
Gabriel Amador <gabrielramador2014@gmail.com>
2021-01-25 16:02:00 -0300

    criando componentes

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,45 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0d47a1",
+    secondary: "#29b6f6",
+    mainBg: "#1C1814",
+    contrastText: "#FFFFFF",
+    wrong: "#FF5722",
+    success: "#4CAF50"
   },
 }
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,50 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import Widget from "../src/components/Widget/index"; 
+import Footer from "../src/components/Footer/index";
+import GitHubCorner from "../src/components/GithubCorner/index";
+import QuizBackground from "../src/components/QuizBackground/index";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={'https://a-static.besthdwallpaper.com/the-legend-of-zelda-breath-of-the-wild-para-o-switch-nintendo-papel-de-parede-1280x768-922_13.jpg'} >
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1> The Legend Of Zelda </h1>
+          </Widget.Header>
+          
+          <Widget.Content>
+            <p>
+              Teste os seus conhecimentos sobre Zelda e divirta-se criando o seu
+              AluraQuiz!
+            </p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <p>
+              Teste os seus conhecimentos sobre Zelda e divirta-se criando o seu
+              AluraQuiz!
+            </p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+
+      <GitHubCorner projectUrl="https://github.com/amador2014" />
+    </QuizBackground>
+  );
 }
